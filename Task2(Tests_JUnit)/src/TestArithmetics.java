e1e0ae518c150af66e1438ffede415e08fab21f0
hellupp <77292360+hellupp@users.noreply.github.com>
2021-06-28 14:55:09 +0300

    Task2(Tests_JUnit) in progress

---
+++ Task2(Tests_JUnit)/src/TestArithmetics.java
@@ -0,0 +1,39 @@
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestArithmetics {
+    private static Arithmetics arithmetics;
+    private final double delta = 0.0000000000001;
+
+    @BeforeClass
+    public static void init() {
+        arithmetics = new Arithmetics();
+    }
+
+    @Test
+    public void testAdd() {
+        double result = arithmetics.add(3, 7);
+        Assert.assertEquals(10.0, result, delta);
+    }
+
+    @Test
+    public void testDeduct() {
+        double result = arithmetics.deduct(3, 7);
+        Assert.assertEquals(-4.0, result, delta);
+    }
+
+    @Test
+    public void testMult() {
+        double result = arithmetics.mult(3, 7);
+        Assert.assertEquals(21.0, result, delta);
+    }
+
+    @Test
+    public void TestDiv() {
+        double result = arithmetics.div(10, 5);
+        Assert.assertEquals(2.0, result, delta);
+    }
+
+
+}
