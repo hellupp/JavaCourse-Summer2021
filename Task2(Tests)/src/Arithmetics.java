f0049a7ff02fe9e301d7713b900675d905722b69
hellupp <77292360+hellupp@users.noreply.github.com>
2021-06-30 17:15:31 +0300

    Task2(Tests) done

---
+++ Task2(Tests)/src/Arithmetics.java
@@ -0,0 +1,22 @@
+public class Arithmetics {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double deduct(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+
+        return a / b;
+    }
+}
