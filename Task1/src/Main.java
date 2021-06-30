de08666c5f349c339a81f0e607a289c9ed442a1b
hellupp <77292360+hellupp@users.noreply.github.com>
2021-06-30 17:12:46 +0300

    Task1 done

---
+++ Task1/src/Main.java
@@ -0,0 +1,11 @@
+// Main class
+public class Main {
+
+    public static void main(String[] args) {
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
+
+        controller.processUser();
+    }
+}
