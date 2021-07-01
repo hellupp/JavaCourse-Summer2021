b6a053413ae56dc51cd52235e46bbf143c438ada
hellupp <77292360+hellupp@users.noreply.github.com>
2021-07-01 11:54:24 +0300

    Task2(Game)  ->  JUnit tests added

--- Task2(Game)/src/GameTest.java
+++ Task2(Game)/src/GameTest.java
@@ -12,12 +12,22 @@ public class GameTest {
     }
 
     @Test
-    public void guessedNumbersShouldBeInRange() {
+    public void secretNumbersIsInDiapason() {
         for (int i = 0; i < 10000; i++) {
             model.generateSecretNumber();
             int secretNumber = model.getSecretNumber();
             boolean isInDiapason = (secretNumber > model.getMinBarrier()) & (secretNumber < model.getMaxBarrier());
             Assert.assertTrue(isInDiapason);
+        }
+    }
+
+    @Test
+    public void equalledSecretNumberReturnFalse() {
+        for (int i = 0; i < 10000; i++) {
+            model.getAttempts().clear();
+            model.generateSecretNumber();
+            int secretNumber = model.getSecretNumber();
+            Assert.assertFalse(model.checkEnteredNumber(secretNumber));
         }
     }
 }
