f0827800e0d19688b66a8af75f780e279674789f
hellupp <77292360+hellupp@users.noreply.github.com>
2021-07-12 17:44:32 +0300

    Task4  ->  improved

--- Task4(ImmutableObject)/src/GradesBook.java
+++ Task4(ImmutableObject)/src/GradesBook.java
@@ -4,7 +4,7 @@ public final class GradesBook {
     private final double averageMark;
 
     public GradesBook(Student student, int course, double averageMark) {
-        this.student = student;
+        this.student = new Student(student.getName(), student.getAge(), student.getGroup());
         this.course = course;
         this.averageMark = averageMark;
     }
@@ -19,5 +19,17 @@ public final class GradesBook {
 
     public double getAverageMark() {
         return averageMark;
+    }
+
+    public GradesBook setStudent(Student student) {
+        return new GradesBook(student, this.course, this.averageMark);
+    }
+
+    public GradesBook setCourse(int course) {
+        return new GradesBook(this.student, course, this.averageMark);
+    }
+
+    public GradesBook setAverageGrade(double avgMark) {
+        return new GradesBook(this.student, this.course, avgMark);
     }
 }
--- Task4(ImmutableObject)/src/Main.java
+++ Task4(ImmutableObject)/src/Main.java
@@ -5,5 +5,16 @@ public class Main {
 
         System.out.println(gradesBook.getStudent());
         System.out.println(student);
+
+        System.out.println("----------------------");
+
+        gradesBook.setCourse(3);
+        gradesBook.setStudent(new Student("Galya", 24, "IPZ34"));
+        System.out.println(gradesBook.getStudent());
+
+        System.out.println("----------------------");
+
+        student.setAge(23);
+        System.out.println(gradesBook.getStudent());
     }
 }
